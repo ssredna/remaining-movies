36ba4e2d8fdfec91b6e3c83dea8a1cdcd97dc51b
Anders Solberg <ssredna@gmail.com>
2023-04-27 17:16:16 +0200

    add css for centering and colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,38 @@
 	export let data;
 </script>
 
-<div>
-	<h1>Remaining movies</h1>
-
-	<p>How many movies do I have left until 1000?</p>
-
-	<pre>{data.remainingMovies}</pre>
+<div class="container">
+	<h1 class="number">{data.remainingMovies}</h1>
+	<p class="text">filmer igjen til jeg har sett 1000</p>
 </div>
+
+<style>
+	.container {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.number {
+		color: rgb(150, 54, 70);
+		font-size: 8rem;
+	}
+
+	.text {
+		color: rgb(150, 54, 70);
+		font-size: 1.5rem;
+	}
+
+	:global(html, body) {
+		height: 100%;
+		width: 100%;
+		padding: 0;
+		margin: 0;
+	}
+
+	:global(body) {
+		background-color: rgb(255, 188, 133);
+	}
+</style>
