b247e1d00ce32d18edfe2eb0c23d1fc6834d9d43
Anders Solberg <ssredna@gmail.com>
2023-05-02 00:58:16 +0200

    make results go away when not focused

--- src/routes/Suggestions.svelte
+++ src/routes/Suggestions.svelte
@@ -4,43 +4,65 @@
 
 	export let searchResults: movie[] | undefined;
 
+	let isFocused = true;
+
 	function posterUrlFromPosterPath(posterPath: string) {
 		return `https://image.tmdb.org/t/p/w92${posterPath}`;
 	}
 </script>
 
-<form method="POST" action="?/search">
-	<h2>Foreslå film nr 1000</h2>
-	<input type="search" name="q" />
-</form>
+<div class="container">
+	<form method="POST" action="?/search">
+		<h2>Foreslå film nr 1000</h2>
+		<input
+			type="search"
+			name="q"
+			autofocus
+			autocomplete="off"
+			on:focusout={() => (isFocused = false)}
+			on:focusin={() => (isFocused = true)}
+		/>
+	</form>
 
-{#if searchResults}
-	{#each searchResults as movie}
-		<div class="result-item">
-			<div>
-				<object
-					data={posterUrlFromPosterPath(movie.poster_path)}
-					type="image/jpeg"
-					class="poster"
-					title={movie.title}
-				>
-					<img src={noImage} alt="Name" class="poster" />
-				</object>
-			</div>
-			<div class="result-item-text">
-				{@html movie.title}
-			</div>
+	{#if searchResults && isFocused}
+		<div class="search-results">
+			{#each searchResults as movie}
+				<div class="result-item">
+					<div>
+						<object
+							data={posterUrlFromPosterPath(movie.poster_path)}
+							type="image/jpeg"
+							class="poster"
+							title={movie.title}
+						>
+							<img src={noImage} alt="Name" class="poster" />
+						</object>
+					</div>
+					<div class="result-item-text">
+						{@html movie.title}
+					</div>
+				</div>
+			{/each}
 		</div>
-	{/each}
-{/if}
+	{/if}
+</div>
 
 <style>
+	.container {
+		position: relative;
+	}
+
 	h2 {
 		color: rgb(150, 54, 70);
 		font-size: 1.5rem;
 	}
 
 	input {
+		width: 100%;
+	}
+
+	.search-results {
+		position: absolute;
 		width: 100%;
 	}
 
