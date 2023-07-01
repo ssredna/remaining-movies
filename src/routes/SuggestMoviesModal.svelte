2eecbf241e0ce44f3764dfdd348fe3ca73aec938
Anders Solberg <ssredna@gmail.com>
2023-07-01 19:10:35 +0200

    add basic modal

---
+++ src/routes/SuggestMoviesModal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let modal: HTMLDialogElement;
+
+	onMount(() => {
+		modal.showModal();
+	});
+</script>
+
+<dialog bind:this={modal} on:close>hello</dialog>
+
+<style>
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.8);
+	}
+</style>
