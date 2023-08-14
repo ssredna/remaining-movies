24d235480fac01fc33c6d5e0d5b53f41812a4ca4
Anders Solberg <ssredna@gmail.com>
2023-08-14 18:49:45 +0200

    fetch movies from tmdb from modal

--- src/routes/search/+server.ts
+++ src/routes/search/+server.ts
@@ -1,6 +1,17 @@
+import { VITE_TMDB_CLIENT_ID } from '$env/static/private';
 import { json } from '@sveltejs/kit';
+import type { movie } from '../+page.server';
 
-export const GET = async ({ url }) => {
+export async function GET({ url }) {
 	const searchString = url.searchParams.get('q') ?? '';
-	return json(searchString.toUpperCase());
-};
+
+	const tmdbQuery =
+		`https://api.themoviedb.org/3/search/movie?api_key=${VITE_TMDB_CLIENT_ID}&query=` +
+		encodeURIComponent(searchString);
+
+	const movies: movie[] = await fetch(tmdbQuery)
+		.then((res) => res.json())
+		.then((res) => res.results);
+
+	return json(movies);
+}
