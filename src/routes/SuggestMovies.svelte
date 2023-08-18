46e91d31711c119eed0605b9090d2a078f41aab7
Anders Solberg <ssredna@gmail.com>
2023-08-18 18:07:09 +0200

    remove old suggest in favor of modal-suggest

--- src/routes/SuggestMovies.svelte
+++ src/routes/SuggestMovies.svelte
@@ -1,18 +1,12 @@
 <script lang="ts">
+	import SuggestButton from './SuggestButton.svelte';
 	import SuggestMoviesModal from './SuggestMoviesModal.svelte';
 
 	let showModal = false;
 </script>
 
-<button on:click={() => (showModal = true)}>Foreslå filmer</button>
+<SuggestButton value="Foreslå film" on:click={() => (showModal = true)} />
 
 {#if showModal}
 	<SuggestMoviesModal on:close={() => (showModal = false)} />
 {/if}
-
-<style>
-	button {
-		border: 1px solid black;
-		padding: 0.5rem;
-	}
-</style>
